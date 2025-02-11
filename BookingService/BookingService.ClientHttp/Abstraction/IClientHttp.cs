e96071ab7d723c9a94465ee2526782327d0e05ca
kevinjudo <casokevin02@gmail.com>
2025-02-11 21:44:35 +0100

    Update IClientHttp.cs

--- BookingService/BookingService.ClientHttp/Abstraction/IClientHttp.cs
+++ BookingService/BookingService.ClientHttp/Abstraction/IClientHttp.cs
@@ -3,8 +3,9 @@ using BookingService.Shared;
 
 namespace BookingService.ClientHttp.Abstraction
 {
+    // Interfaccia per la gestione delle chiamate HTTP ai microservizi
     public interface IClientHttp
     {
-        Task<UserDto?> GetUserByIdAsync(int userId);
+        Task<UserDto?> GetUserByIdAsync(int userId); // Metodo per ottenere un utente tramite ID dal servizio utenti
     }
 }
