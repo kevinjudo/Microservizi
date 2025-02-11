58567d35e62e3b4e0a2ea44ca9f01ded853e9900
kevinjudo <casokevin02@gmail.com>
2025-02-11 21:40:46 +0100

    Update ClientHttp.cs

--- BookingService/BookingService.ClientHttp/ClientHttp.cs
+++ BookingService/BookingService.ClientHttp/ClientHttp.cs
@@ -6,24 +6,27 @@ using BookingService.ClientHttp.Abstraction;
 
 namespace BookingService.ClientHttp
 {
+// Implementazione della classe per la gestione delle chiamate HTTP al servizio utenti
     public class ClientHttp : IClientHttp
     {
         private readonly HttpClient _httpClient;
-
+        // Costruttore che inietta HttpClient per effettuare le richieste HTTP
         public ClientHttp(HttpClient httpClient)
         {
             _httpClient = httpClient;
         }
-
+        // Metodo per ottenere i dettagli di un utente tramite il suo ID
         public async Task<UserDto?> GetUserByIdAsync(int userId)
         {
+            // Esegue una richiesta HTTP GET per ottenere le informazioni dell'utente
             var response = await _httpClient.GetAsync($"/api/User/ReadUser/{userId}");
             if (response.IsSuccessStatusCode)
             {
+                // Se la richiesta ha successo, deserializza la risposta JSON in un oggetto UserDto
                 var content = await response.Content.ReadAsStringAsync();
                 return JsonSerializer.Deserialize<UserDto>(content, new JsonSerializerOptions { PropertyNameCaseInsensitive = true });
             }
-
+            // Restituisce null se la richiesta non è andata a buon fine
             return null;
         }
     }
